33be1b6303c1fc712b9bf486ae41498f65adee98
Rohman Aditiya <74638607+rohmxx@users.noreply.github.com>
2022-12-24 10:32:46 +0700

    Add files via upload

---
+++ bno080.c
@@ -0,0 +1,33 @@
+#include "GlobalVariables.h"
+
+uint8_t msg_buffer[50] = {0};
+uint16_t yaw_data, yaw_lsb, yaw_msb;
+float yaw;
+
+void BNO080_DMA_init(){
+	HAL_UART_Receive_DMA(&huart1, msg_buffer, 50);
+}
+
+void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart)
+{
+  HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) 
+{
+	for(int i=0; i<50; i++){
+		if(msg_buffer[i]==0xAA && msg_buffer[i+1]==0xAA){
+			yaw_lsb		= msg_buffer[i+3];
+			yaw_msb		= msg_buffer[i+4];
+			yaw_data	= yaw_msb<<8 | yaw_lsb;
+			if(yaw_data>=0xB9B0){
+				yaw			= 360 + (((int)yaw_data - 0xFFFF) / 100);
+			}
+			else{
+				yaw			= yaw_data / 100;
+			}
+			break;
+		}
+	}
+	HAL_UART_Receive_DMA(&huart1, msg_buffer, 50);
+}
